38134803b1c1dca7c5877c7e4497d640eaf85cfa
rofina-j <rofinar4@gmail.com>
2025-01-22 09:48:49 +0530

    final commit

--- data.py
+++ data.py
@@ -4,5 +4,10 @@ app=Flask(__name__)
 def get_data():
     return jsonify({"msg":"this is my first",
                     "data":"one"})
+@app.route('/point',methods=['POST'])
+def post_data():
+    data=request.get_json()
+    return jsonify({"msg":"data received",
+                   "data":data})
 if __name__=='__main__':
     app.run(debug=True)
