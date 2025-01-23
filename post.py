162265c75dd3c6773b6578063159858e3ad8bf08
rofina-j <rofinar4@gmail.com>
2025-01-23 19:40:18 +0530

    post method for name,age,gender,city

--- post.py
+++ post.py
@@ -3,25 +3,28 @@ app=Flask(__name__)
 unique=[]
 @app.route('/one',methods=['POST'])
 def post_data():
+
     data=request.get_json()
+
+    
     name=data.get('name')
     age=data.get('age')
     gender=data.get('gender')
     city=data.get('city')
+
+    if not name and not age and not gender and not city:
+        return jsonify({"msg":"missing field"})
+    
     for i in unique:
         if i['name']==name:
             return jsonify({"msg":"error name alreafy exist"})
     new={"name":name,
-
-
-    "age":age,
+         "age":age,
     "gender":gender,
     "city":city}
+
     unique.append(new)
+
     return jsonify({"msg":"success","msg":new})
-if __name__=="_main__":
+if __name__=='__main__':
     app.run(debug=True)
-
-
-
-
