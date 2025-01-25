0c4d3030867344ab3d2c6e84951055c688515619
rofina-j <rofinar4@gmail.com>
2025-01-25 20:06:15 +0530

    show city count

--- post.py
+++ post.py
@@ -1,19 +1,24 @@
 from flask import Flask,request,jsonify
 app=Flask(__name__)
 users=[
-    {"id":"1","name":"anu","gender":"f"},
-    {"id":"2","name":"anbu","gender":"m"},
-    {"id":"3","name":"anish","gender":"m"},
-    {"id":"4","name":"anusha","gender":"f"},
-    {"id":"5","name":"anil","gender":"m"},
+    {"id":1,"name":"anu","gender":"f","age":24,"city":"chn"},
+    {"id":2,"name":"anbu","gender":"m","age":45,"city":"bgl"},
+    {"id":3,"name":"anish","gender":"m","age":22,"city":"chn"},
+    {"id":4,"name":"anusha","gender":"f","age":33,"city":"bgl"},
+    {"id":5,"name":"anil","gender":"m","age":56,"city":"delhi"}
 ]
 @app.route('/one',methods=['GET'])
 def get_data():
-    gender=request.args.get('gender')
-    c=0
+    minage=int(request.args.get('minage'))
+    maxage=int(request.args.get('maxage'))
+    count={}
     for i in users:
-        if i['gender']==gender:
-            c+=1
-    return jsonify({"gender": gender, "count": c})
+        if minage <= i["age"] <=maxage:
+            city=i["city"]
+            if city in count:
+                count[city]+=1
+            else:
+                count[city]=1
+    return jsonify(count)
 if __name__ == '__main__':
     app.run(debug=True)
