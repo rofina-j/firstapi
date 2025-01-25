bdba64faece598e1e60ef6c5207220ea55df48d8
rofina-j <rofinar4@gmail.com>
2025-01-25 19:31:30 +0530

    count gender

--- post.py
+++ post.py
@@ -1,43 +1,19 @@
 from flask import Flask,request,jsonify
 app=Flask(__name__)
-unique=[]
-@app.route('/one',methods=['POST'])
-def post_data():
-
-    data=request.get_json()
-
-    
-    name=data.get('name')
-    age=data.get('age')
-    gender=data.get('gender')
-    city=data.get('city')
-
-    if not name or not age or not gender or not city:
-        return jsonify({"msg":"missing field"})
-    
-    for i in unique:
-        if i['name']==name:
-            return jsonify({"msg":"error name alreafy exist"})
-    new={"name":name,
-         "age":age,
-    "gender":gender,
-    "city":city}
-
-    unique.append(new)
-
-    return jsonify({"msg":"success","data":new})
-@app.route('/details',methods=['GET'])
+users=[
+    {"id":"1","name":"anu","gender":"f"},
+    {"id":"2","name":"anbu","gender":"m"},
+    {"id":"3","name":"anish","gender":"m"},
+    {"id":"4","name":"anusha","gender":"f"},
+    {"id":"5","name":"anil","gender":"m"},
+]
+@app.route('/one',methods=['GET'])
 def get_data():
-    name=request.args.get('name')
-    for i in unique:
-        if i['name']==name:
-            details = {
-                "name": i["name"],
-                "age": i["age"],
-                "gender": i["gender"],
-                "city":i["city"]
-            }
-        return jsonify({"data":details})
-    return jsonify({"error": "not found"})
-if __name__=='__main__':
+    gender=request.args.get('gender')
+    c=0
+    for i in users:
+        if i['gender']==gender:
+            c+=1
+    return jsonify({"gender": gender, "count": c})
+if __name__ == '__main__':
     app.run(debug=True)
